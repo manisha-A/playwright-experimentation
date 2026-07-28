486bb2a8b2fdc063df08e4277bcb4df7a650f6a5
Manisha Awasthi <manisha@192.168.1.244>
2026-07-28 23:47:30 +0100

    using fixtures

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { test as base, expect, Page } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+type Fixtures = {
+  homePage: Page;
+}
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+const test = base.extend<Fixtures>({
+  homePage: async ({page}, use) => {
+    await page.goto('https://playwright.dev/', {waitUntil: 'domcontentloaded'});
+    await use(page);
+  },
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+
+test('has title', async ({ homePage }) => {
+  // Expect a title "to contain" a substring.
+  await expect(homePage).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ homePage }) => {
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await homePage.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(homePage.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
